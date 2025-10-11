cda59896edfba4cfc37cd11ad52b2d6cddb6bdd6
Sarvesh-7777 <sonawanesarvesh19@gmail.com>
2025-10-11 13:55:37 -0500

    redesign contact page

--- client-app/src/Components/Contact.tsx
+++ client-app/src/Components/Contact.tsx
@@ -1,65 +1,75 @@
 import React from 'react';
 
-const Contact = () => {
-    const containerStyle: React.CSSProperties = {
-        backgroundColor: '#000000',
-        backgroundImage:
-            'repeating-linear-gradient(45deg, #111 0px, #111 2px, #000 2px, #000 4px)',
-        color: '#ffffff',
-        padding: '3rem 1rem',
-        fontFamily: 'Georgia, serif',
-    };
-
-    const headingStyle: React.CSSProperties = {
-        color: '#00BFFF',
-        fontSize: '2.5rem',
-        fontWeight: 'bold',
-        marginBottom: '1.5rem',
-    };
-
-    const paragraphStyle: React.CSSProperties = {
-        fontSize: '1.2rem',
-        maxWidth: '800px',
-        marginBottom: '1.5rem',
-    };
-
-    const listStyle: React.CSSProperties = {
-        fontSize: '1.2rem',
-        marginLeft: '1.5rem',
-        marginBottom: '1rem',
-    };
-
-    const italicTextStyle: React.CSSProperties = {
-        fontSize: '1.2rem',
-        fontStyle: 'italic',
-    };
-
+const Contact: React.FC = () => {
     return (
-        <div style={containerStyle}>
-            <h1 style={headingStyle}>WE’D LOVE TO HEAR FROM YOU.</h1>
-            <p style={paragraphStyle}>
-                Do you want to support our programs? Do you have an idea for a
-                new way for us to make even more dreams come true? We’d love to
-                hear from you.
-            </p>
-            <ul style={listStyle}>
-                <li>
-                    Contact us at:{' '}
-                    <a
-                        href="mailto:info@bworks.org"
-                        style={{ color: '#00BFFF' }}
-                    >
-                        info@bworks.org
-                    </a>
-                </li>
-                <li>
-                    Phone: <span>314-664-0828</span>
-                </li>
-            </ul>
-            <p style={italicTextStyle}>
-                You can also stop by and see us in person on{' '}
-                <strong>Tuesday / Thursday or Saturday.</strong>
-            </p>
+        <div className="login-container">
+            {/* Left side - Contact Info */}
+            <div className="login-left">
+                <h1
+                    className="login-label"
+                    style={{
+                        fontSize: '2rem',
+                        fontWeight: 'bold',
+                        color: '#222',
+                    }}
+                >
+                    WE’D LOVE TO HEAR FROM YOU.
+                </h1>
+                <p
+                    style={{
+                        marginTop: '2rem',
+                        fontSize: '1.1rem',
+                        color: '#333',
+                    }}
+                >
+                    Do you want to support our programs? Do you have an idea for
+                    a new way for us to make even more dreams come true? We’d
+                    love to hear from you.
+                </p>
+                <ul
+                    style={{
+                        fontSize: '1.1rem',
+                        marginLeft: '1.5rem',
+                        marginBottom: '1rem',
+                        color: '#222',
+                    }}
+                >
+                    <li>
+                        Contact us at:{' '}
+                        <a
+                            href="mailto:info@bworks.org"
+                            style={{
+                                color: '#1c7ed6',
+                                textDecoration: 'underline',
+                            }}
+                        >
+                            info@bworks.org
+                        </a>
+                    </li>
+                    <li>
+                        Phone: <span>314-664-0828</span>
+                    </li>
+                </ul>
+                <p
+                    style={{
+                        fontSize: '1.1rem',
+                        fontStyle: 'italic',
+                        color: '#333',
+                    }}
+                >
+                    You can also stop by and see us in person on{' '}
+                    <strong>Tuesday / Thursday or Saturday.</strong>
+                </p>
+            </div>
+            {/* Right side - Overlapping Circles */}
+            <div className="login-right">
+                <div className="circle large">
+                    <img src="/cycle.jpg" alt="BWorks bike" />
+                </div>
+                <div className="circle small">
+                    <img src="/image.jpg" alt="BWorks kids" />
+                </div>
+            </div>
         </div>
     );
 };
